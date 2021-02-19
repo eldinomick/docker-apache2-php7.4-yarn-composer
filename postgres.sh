379b15e46d516ca732142312a75b0b70896b6483
michael <michael@cadot.info>
2021-02-19 19:02:34 +0100

    max upload

--- postgres.sh
+++ postgres.sh
@@ -1,4 +1,4 @@
-docker run --name postgres -p 5432 \
+docker run --name postgres -p 5432:5432 \
 --restart always \
 -e POSTGRES_PASSWORD=password \
 -v /home/docker/postgres:/var/lib/postgresql/data \
